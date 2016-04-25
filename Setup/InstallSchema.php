cf224a320cdec9dc05c23d4fc9eec570c5f42170
Gabriel Somoza <gabriel@somoza.me>
2016-04-25 14:17:58 +0200

    Add InstallSchema script

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace MagentoHackathon\MagentoMigrations;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+/**
+ * Class InstallSchema
+ *
+ * @author Gabriel Somoza <gabriel.somoza@cu.be>
+ */
+final class InstallSchema
+{
+    /** Name for the migrations table */
+    const MIGRATIONS_TABLE = 'migrations';
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+        $connection = $installer->getConnection();
+
+        $table = new Table();
+        $table->addColumn(
+            'id',
+            Table::TYPE_TEXT,
+            null, [
+                'nullable' => false,
+                'primary' => true,
+            ]
+        );
+        $table->addColumn('migrated_at', Table::TYPE_DATETIME);
+
+        $connection->createTable($table);
+        $installer->endSetup();
+    }
+}
